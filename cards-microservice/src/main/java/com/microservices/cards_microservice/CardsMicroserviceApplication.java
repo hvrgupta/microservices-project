75f3751ee26b05fe0d145f16cfd000e91c7f45b4
Harsh Gupta <hgupta7@umd.edu>
2024-12-21 22:56:16 -0500

    added different ways to build images

--- cards-microservice/src/main/java/com/microservices/cards_microservice/CardsMicroserviceApplication.java
+++ cards-microservice/src/main/java/com/microservices/cards_microservice/CardsMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.microservices.cards_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class CardsMicroserviceApplication {
 
 	public static void main(String[] args) {
--- loans-microservice/src/main/java/com/microservices/loans_microservice/LoansMicroserviceApplication.java
+++ loans-microservice/src/main/java/com/microservices/loans_microservice/LoansMicroserviceApplication.java
@@ -1,9 +1,27 @@
 package com.microservices.loans_microservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice",
+				description = "Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Harsh Gupta",
+						email = "harshhgupta16@gmail.com"
+				)
+		)
+)
 public class LoansMicroserviceApplication {
 
 	public static void main(String[] args) {
